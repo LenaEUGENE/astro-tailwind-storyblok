63f9e3e1b28e94633501d663307c91d95c97c702
Lena EUGENE <lenaeugene@gmail.com>
2023-02-25 15:31:41 +0100

    Customised Tailwind files, changed layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+        serif: ["var(--font-serif)", ...defaultTheme.fontFamily.serif],
+        heading: ["var(--font-heading)", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
